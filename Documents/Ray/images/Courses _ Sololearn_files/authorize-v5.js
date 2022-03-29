3f0df256f6673e8bc104c3fd1f1ac830a152214a
rehemae <rehemaephraimnanda@gmail.com>
2022-03-29 07:39:53 +0300

    first

---
+++ Documents/Ray/images/Courses _ Sololearn_files/authorize-v5.js
@@ -0,0 +1,25 @@
+﻿//(function () {
+//    function hasOwin() {
+//        return document.cookie.indexOf('LoginState=1') > -1;
+//    }
+
+//    function hasTokens() {
+//        var user = JSON.parse(localStorage.getItem('user'));
+//        var accessToken = JSON.parse(localStorage.getItem('accessToken'));
+//        return !!(user && user.data && accessToken && accessToken.data);
+//    }
+
+//    if (
+//        (hasOwin() && !hasTokens()) || (hasTokens() && !hasOwin())
+//    ) {
+//        localStorage.removeItem('user');
+//        localStorage.removeItem('accessToken');
+//        localStorage.removeItem('refreshToken');
+//        localStorage.removeItem('isDark');
+//        localStorage.removeItem('expiresIn');
+//        localStorage.removeItem('msAccessToken');
+//        localStorage.removeItem('serviceAccessToken');
+//        window.location.href = "/User/Logout?returnUrl=" + window.location.pathname;
+//    }
+//})();
+
